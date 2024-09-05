40eb7390019ae019013125527693b6cab6e3ed60
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-09-05 14:05:53 +0300

    refactor(MAIN): reorganized

--- SRC/DOSBROT.C
+++ SRC/DOSBROT.C
@@ -1,40 +1,89 @@
 #include "VGA.H"
+#include <CONIO.H>
 
 #define WIDTH 320
 #define HEIGHT 200
+#define ITERATION 100
 
+static double minReal;
+static double maxReal;
+static double minImag;
+static double maxImag;
+	
+static double dx;
+static double dy;
+static int value, isRunning;
+
+void init(void);
+void processInput(void);
+void update(void);
+void render(void);
 int computeMandelbrot(double real, double imag, int iteration);
 
 void main(void) {
-    int i,j,value;
-    const double minReal = -2.0;
-    const double maxReal = 1.0;
-    const double minImag = -1.0;
-    const double maxImag = 1.0;
+	init();
+
+	while(isRunning) {
+		processInput();
+		update();
+		render();
+	}
+
+	exitvga();
+}
+
+void init(void) {
+	minReal = -2.0;
+	maxReal = 1.0;
+	minImag = -1.0;
+	maxImag = 1.0;
+		
+	dx = (maxReal - minReal)/(WIDTH - 1);
+	dy = (maxImag - minImag)/(HEIGHT - 1);
+
+	isRunning = 1;
+
+	initvga();
+}
+
+void processInput(void) {
+	if (kbhit()) {
+		char ch;
+		ch = getch();
+			
+		if (ch == (char)0x1b) {
+			isRunning = 0;
+		}
+	}
+}
+
+void update(void) {
+	int i,j;
 	
-	const double dx = (maxReal - minReal)/(WIDTH - 1);
-	const double dy = (maxImag - minImag)/(HEIGHT - 1);
-    
-    _initMode(MODE_VGA_13H);
+	clroffscreen(BLACK);
 	
 	for (i = 0; i < HEIGHT; i++) {
 		for (j = 0; j < WIDTH; j++) {
-			value = computeMandelbrot(minReal + j * dx, maxImag - i * dy, 100);
+			value = computeMandelbrot(minReal + j * dx, maxImag - i * dy, ITERATION);
 			
-			(value == 100) 	? _putpixel(j, i, BLACK): 
-			(value > 90) 	? _putpixel(j, i, WHITE): 
-			(value > 70) 	? _putpixel(j, i, LIGHTRED): 
-			(value > 50) 	? _putpixel(j, i, RED): 
-			(value > 30) 	? _putpixel(j, i, YELLOW): 
-			(value > 20) 	? _putpixel(j, i, LIGHTGREEN): 
-			(value > 10) 	? _putpixel(j, i, GREEN): 
-			(value > 5) 	? _putpixel(j, i, LIGHTCYAN):
-			(value > 4) 	? _putpixel(j, i, CYAN): 
-			(value > 3) 	? _putpixel(j, i, LIGHTBLUE): 
-			(value > 2) 	? _putpixel(j, i, BLUE): 
-			(value > 1) 	? _putpixel(j, i, MAGENTA):_putpixel(j, i, LIGHTMAGENTA); 
+			(value == 100) 	? putpixel(j, i, BLACK): 
+			(value > 90) 	? putpixel(j, i, WHITE): 
+			(value > 70) 	? putpixel(j, i, LIGHTRED): 
+			(value > 50) 	? putpixel(j, i, RED): 
+			(value > 30) 	? putpixel(j, i, YELLOW): 
+			(value > 20) 	? putpixel(j, i, LIGHTGREEN): 
+			(value > 10) 	? putpixel(j, i, GREEN): 
+			(value > 5) 	? putpixel(j, i, LIGHTCYAN):
+			(value > 4) 	? putpixel(j, i, CYAN): 
+			(value > 3) 	? putpixel(j, i, LIGHTBLUE): 
+			(value > 2) 	? putpixel(j, i, BLUE): 
+			(value > 1) 	? putpixel(j, i, MAGENTA):putpixel(j, i, LIGHTMAGENTA); 
 		}
 	}
+}
+
+void render(void) {
+	updateVRAM();
 }
 
 int computeMandelbrot(double real, double imag, int iteration) {
